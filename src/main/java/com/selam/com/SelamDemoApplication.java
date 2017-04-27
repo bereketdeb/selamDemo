1b4dbcf934cb7d5e42c39fe1c7cdac524eadfeaf
bereketdeb <bereketdeb@gmail.com>
2017-04-27 16:58:29 -0500

    first time commite

---
+++ src/main/java/com/selam/com/SelamDemoApplication.java
@@ -0,0 +1,12 @@
+package com.selam.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelamDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelamDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/selam/com/SelamDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.selam.com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelamDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
