7ac9e56bbc3daa69d57a9afb98502a1f371bb528
bereketdeb <bereketdeb@gmail.com>
2017-05-06 13:25:10 -0500

    new project  from intelj

---
+++ src/main/java/com/selamdemo/SelamdemoApplication.java
@@ -0,0 +1,12 @@
+package com.selamdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelamdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelamdemoApplication.class, args);
+	}
+}
